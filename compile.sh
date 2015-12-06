dd9b2b37e91517f51ae01cc76b8bd8a74fe4e228
Zhang Boyang <zhangboyang.id@gmail.com>
2015-12-06 23:56:59 +0800

    add compile scripts

---
+++ compile.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+SRCLIST=`ls | grep '\.cp*$'`
+TARGETLIST=""
+CXXFLAGS="-Wall -Werror"
+EXECNAME="pj"
+
+function call_compiler()
+{
+    echo "g++ $@"
+    g++ $@
+}
+
+for i in `echo "$SRCLIST"`; do
+    SRC="$i"
+    TARGET=`echo "$SRC" | sed -e 's/\.cp*/\.o/g'`
+    TARGETLIST="$TARGETLIST $TARGET"
+    #echo "compile: $SRC -> $TARGET"
+    call_compiler -c $CXXFLAGS -o "$TARGET" "$SRC"
+done
+call_compiler $CXXFLAGS -o "$EXECNAME" $TARGETLIST
