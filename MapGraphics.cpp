e48d46add97d5f2a0c18140bdb81a958e797ac5f
Zhang Boyang <zhangboyang.id@gmail.com>
2015-12-08 14:10:18 +0800

    add map operation (move, zoom) feature

--- MapGraphics.cpp
+++ MapGraphics.cpp
@@ -1,3 +1,4 @@
+#include <cmath>
 #include <cstdio>
 #include <GL/glut.h>
 #include "common.h"
@@ -8,17 +9,35 @@ using namespace std;
 
 void MapGraphics::target(MapData *md) { MapGraphics::md = md; }
 
-void MapGraphics::init_glut()
-{
-    glClearColor(0.0, 0.0, 0.0, 0.0);
-    glMatrixMode(GL_PROJECTION);
-    gluOrtho2D(0.0, md->gratio, 0.0, 1.0);
-}
-
 void MapGraphics::set_glimit(double gminx, double gmaxx, double gminy, double gmaxy)
 {
     MapGraphics::gminx = gminx; MapGraphics::gmaxx = gmaxx; // set limit for MapGraphics
     MapGraphics::gminy = gminy; MapGraphics::gmaxy = gmaxy;
+}
+
+void MapGraphics::move_glimit(int x, int y)
+{
+    if (x != 0) {
+        double diffx = (gmaxx - gminx) * MOVESTEP * x;
+        gmaxx += diffx;
+        gminx += diffx;
+    }
+    if (y != 0) {
+        double diffy = (gmaxy - gminy) * MOVESTEP * y;
+        gmaxy += diffy;
+        gminy += diffy;
+    }
+}
+
+void MapGraphics::zoom_glimit(int f)
+{
+    double diffx = (gmaxx - gminx) * (1 - pow(ZOOMSTEP, f));
+    gmaxx -= diffx;
+    gminx += diffx;
+    
+    double diffy = (gmaxy - gminy) * (1 - pow(ZOOMSTEP, f));
+    gmaxy -= diffy;
+    gminy += diffy;
 }
 
 void MapGraphics::get_gcoord_by_node(MapNode *node, double *gx, double *gy) // transform for MapGraphics
@@ -28,12 +47,23 @@ void MapGraphics::get_gcoord_by_node(MapNode *node, double *gx, double *gy) // t
     *gy = (node->x - gminx) / (gmaxx - gminx);
 }
 
-static MapGraphics *mgptr;
+void MapGraphics::map_operation(MapGraphicsOperation op)
+{
+    printf("op=%d\n", op);
+    if (op == UP) move_glimit(1, 0);
+    else if (op == DOWN) move_glimit(-1, 0);
+    else if (op == LEFT) move_glimit(0, -1);
+    else if (op == RIGHT) move_glimit(0, 1);
+    else if (op == ZOOMOUT) zoom_glimit(-1);
+    else if (op == ZOOMIN) zoom_glimit(1);
+    else assert(0);
+}
+
+static MapGraphics *mgptr = NULL;
 
 void MapGraphics::redraw()
 {
     //fflush(stdout);
-    set_glimit(md->minx, md->maxx, md->miny, md->maxy); // FIXME: full map
     glClear(GL_COLOR_BUFFER_BIT);
     
     /*for (vector<MapNode *>::iterator it = md->nl.begin(); it != md->nl.end(); it++) {
@@ -64,19 +94,49 @@ void MapGraphics::redraw()
     glFlush();
 }
 
+void MapGraphics::special_keyevent(int key, int x, int y)
+{
+    MapGraphicsOperation op;
+    switch (key) {
+        case GLUT_KEY_UP: op = UP; break;
+        case GLUT_KEY_DOWN: op = DOWN; break;
+        case GLUT_KEY_LEFT: op = LEFT; break;
+        case GLUT_KEY_RIGHT: op = RIGHT; break;
+        case GLUT_KEY_PAGE_UP: op = ZOOMOUT; break;
+        case GLUT_KEY_PAGE_DOWN: op = ZOOMIN; break;
+        default: printf("unknown key %d\n", key); return;
+    }
+    map_operation(op);
+    glutPostRedisplay();
+}
+
 void redraw_wrapper() { assert(mgptr); TIMING("redraw", { mgptr->redraw(); }) }
+void special_keyevent_wrapper(int key, int x, int y) { assert(mgptr); mgptr->special_keyevent(key, x, y); }
+
+void MapGraphics::init_glut()
+{
+    glClearColor(0.0, 0.0, 0.0, 0.0);
+    glMatrixMode(GL_PROJECTION);
+    gluOrtho2D(0.0, md->gratio, 0.0, 1.0);
+}
 
 void MapGraphics::show(const char *title, int argc, char *argv[])
 {
     assert(md);
+    set_glimit(md->minx, md->maxx, md->miny, md->maxy); // FIXME: full map
+    assert(mgptr == NULL);
+    mgptr = this;
+    
+    /* glut things */
     glutInit(&argc, argv);
     glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
     glutInitWindowSize(md->mratio / md->gratio * WINDOWHEIGHT, WINDOWHEIGHT);
     glutCreateWindow(title);
     init_glut();
-    mgptr = this;
+    glutSpecialFunc(special_keyevent_wrapper);
     glutDisplayFunc(redraw_wrapper);
     glutMainLoop();
+    
     mgptr = NULL;
 }
 
--- MapGraphics.h
+++ MapGraphics.h
@@ -9,14 +9,27 @@ class MapGraphics {
     void init_glut();
     
     public:
+    
+    enum MapGraphicsOperation {
+        UP, DOWN, LEFT, RIGHT,
+        ZOOMOUT, ZOOMIN,
+    };
     static const int WINDOWHEIGHT = 500;
+    static const double MOVESTEP = 0.2;
+    static const double ZOOMSTEP = 0.8;
+    
     double gminx, gmaxx, gminy, gmaxy;
     MapData *md;
     
     void target(MapData *md);
     void get_gcoord_by_node(MapNode *node, double *gx, double *gy);
     void set_glimit(double gminx, double gmaxx, double gminy, double gmaxy);
+    void move_glimit(int x, int y);
+    void zoom_glimit(int f);
+    void map_operation(MapGraphicsOperation op);
+    
     void redraw();
+    void special_keyevent(int key, int x, int y);
     void show(const char *title, int argc, char *argv[]);
 };
 #endif
